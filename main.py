f278edc994ed9bc3dab9f053d5628c8a97026d86
Nesbit <nesbitmuretar@gmail.com>
2025-08-02 22:13:50 +0300

    Refactor CLI usage documentation and improve input handling in main.py

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
+"""
+Dojo Room Allocation System
 
+Usage:
+  main.py create_room <room_type> <room_name>...
+  main.py add_person <person_name> <role> [<wants_living_space>]
 
-'''
-DOJO Application CLI Interface
-
-usage:
-create_room <room_type> <room_name>
-add_person <person_name> <role> [<wants_living_space>]
-'''
+Options:
+  -h --help     Show this screen.
+"""
 
 from docopt import docopt
 from dojo import Dojo
 
-if __name__ == '__main__':
-  args= docopt(__doc__)
-  dojo = Dojo()
-  
-  if args['create_room']:
-    for name in args['<room_name>']:
-      print(dojo.create_room(args['<room_type>'], name))
-      
-  elif args['add_person']:
-    name=args['<person_name>']
-    role=args['<role>']
-    wants_living_space=args['<wants_living_space>']or 'No'
-    for line in dojo.add_person(name, role, wants_living_space):
-      print(line)
-      
-  else:
-    print("Invalid input. Use 'create_room' or 'add_person'.")
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    dojo = Dojo()
+
+    if args['create_room']:
+        room_type = args['<room_type>']
+        for name in args['<room_name>']:
+            print(dojo.create_room(room_type, name))
+
+    elif args['add_person']:
+        name = args['<person_name>']
+        role = args['<role>']
+        wants = args['<wants_living_space>'] or 'No'
+        for line in dojo.add_person(name, role, wants):
+            print(line)
